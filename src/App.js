615ec7e7ebc7db4366ab1f822cc226f4f849df48
ciplenka <ciplenka.ok@gmail.com>
2022-12-04 18:44:32 +0200

    started moving app from sandbox

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./styles.css";
+import WeatherApp from "./WeatherApp.js";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App">
+          <div className="container">
+            <div className="card">
+              <div className="card-body">
+                <br />
+                <h1>Weather App</h1>
+                <div className="position-relative">
+                  <div className="position-absolute top-0 end-0"></div>
+                </div>
+                <div className="position-relative">
+                  <div className="position-absolute top-0 start-10 ">
+                    <WeatherApp />
+                  </div>
+                  <div className="position-relative">
+                    <div className="position-absolute top-20 start-50 translate-middle"></div>
+                  </div>
+                </div>
+                <div className="position-absolute top-10 start-10"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="linkSource">
+          <a
+            href="https://github.com/ciplenka/awesome-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-github"></i>{" "}
+          </a>
+          by Viktoriia Zaremba
+        </div>
       </header>
     </div>
   );
---
+++ src/WeatherApp.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./styles.css";
+
+export default function WeatherApp() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "62231151ce343c4d68652e1617efc22f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form id="search-form" onSubmit={handleSubmit}>
+      <input
+        id="search-city"
+        type="search"
+        placeholder="Enter a city.."
+        onChange={updateCity}
+      />
+      <button type="submit" id="submit-button">
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
